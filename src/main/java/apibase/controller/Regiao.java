386721e4d4ad374516a05af203b6348699fe69b0
nananzoka <nunesdossantosnatasha13@gmail.com>
2025-06-27 16:51:02 -0300

    ok

--- src/main/java/apibase/controller/Regiao.java
+++
@@ -1,5 +0,0 @@
-package apibase.controller;
-
-public class Regiao {
-
-}
--- src/main/java/apibase/controller/RegiaoController.java
+++ src/main/java/apibase/controller/RegiaoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import apibase.model.Regiao;
 import apibase.repository.RegiaoRepository;
 @CrossOrigin("*")
 @RestController
